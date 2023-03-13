c1d4a9d8c7be247c078e82e71a8aaa3adaced0bb
AntonMakarkin <amakarkin27@gmail.com>
2023-03-14 00:53:40 +0300

    Pages Navigation & Redirect

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Heading from "@/components/Heading";
+import styles from "../styles/Home.module.scss";
 
 const Home = () => (
-  <Heading text="Something went wrong..."/>
+  <div className={styles.wrapper}>
+    <Heading text="Hello World"/>
+  </div>
 );
 
 export default Home;
